10c88d60991c584671c8b98b0e37f3a41801effa
Sergio Barreras <sergiobg.isc@gmail.com>
2025-12-02 18:39:06 -0600

    Adds font styling and preconnect links for improved performance and typography

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+      },
+    },
   },
   plugins: [
     require('@midudev/tailwind-animations')
